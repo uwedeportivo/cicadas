1317d9fb1c69d00f44570da32c3ea95d57cf4e0e
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2023-12-29 10:24:05 -0800

    overall fixes non-blasts

--- emp/internal/EMPTriggerInternal.hs
+++ emp/internal/EMPTriggerInternal.hs
@@ -6,26 +6,36 @@ maxInListWithIndex :: [Int] -> (Int, Int)
 maxInListWithIndex [] = (0, -1)
 maxInListWithIndex [x] = (x, 0)
 maxInListWithIndex (x:xs) = if x > y then (x, 0) else (y, (i+1))
-  where
-    (y, i) = maxInListWithIndex xs
+    where
+        (y, i) = maxInListWithIndex xs
 
 blasted :: (Vector (Int, [Int])) -> (Int -> Int) -> [Int] -> Int -> (Int, [Int])
 blasted _ _ [] _ = (0, [])
 blasted _ _ _ 0 = (0, [])
-blasted r f xs n = w
+blasted r f xs n = (mp, (n:iz))
     where
         x = xs !! (n - 1)
         g :: (Vector (Int, [Int])) -> Int -> Int
         g rr j = ((fst (rr!j))) + (min x (f (n - j)))
         ps = [(g r j) | j <- [0..(n-1)]]
         (mp, i) = maxInListWithIndex ps
-        v = fst (r!(n-1))
         iz = snd (r!i)
-        w = if mp > v then (mp, (n:iz)) else (r!(n-1))
 
-blastsImpl :: [Int] -> (Int -> Int) -> (Int, [Int])
-blastsImpl [] _ = (0, [])
-blastsImpl xs f = r ! n
-  where
-    n = length xs
-    r = generate ((length xs) + 1) (blasted r f xs)
+blastsImpl :: [Int] -> (Int -> Int) -> (Vector (Int, [Int]))
+blastsImpl xs f = r
+    where
+        r = generate ((length xs) + 1) (blasted r f xs)
+
+overall :: (Vector (Int, [Int])) -> (Vector (Int, [Int])) -> Int -> (Int, [Int])
+overall _ _  0 = (0, [])
+overall o r n = if b > c then (b, biz) else (c, oiz)
+    where
+        (b, biz) = r ! n
+        (c, oiz) = o ! (n - 1)
+
+overallImpl :: [Int] -> (Int -> Int) -> (Int, [Int])
+overallImpl xs f = o ! n
+    where
+        n = length xs
+        r = blastsImpl xs f
+        o = generate ((length xs) + 1) (overall o r)
--- emp/src/EMPTrigger.hs
+++ emp/src/EMPTrigger.hs
@@ -3,4 +3,4 @@ module EMPTrigger (blasts) where
 import EMPTriggerInternal
 
 blasts :: [Int] -> (Int -> Int) -> (Int, [Int])
-blasts xs f = blastsImpl xs f
+blasts xs f = overallImpl xs f
